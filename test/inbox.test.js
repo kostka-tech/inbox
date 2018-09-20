5b9d9031501ae1ee0b00737c4046faee6f4e3bdb
Kostka Tech <kostka.tech@gmail.com>
2018-09-20 12:58:51 +0200

    My first smart contract :) A cutting edge inbox...

---
+++ test/inbox.test.js
@@ -0,0 +1,34 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hello world!! :D ']})
+        .send({ from: accounts[0], gas: '1000000' });
+});
+
+describe('inbox', () => {
+    it('should deploy a contract', () => {
+        assert.ok(inbox.options.address);
+    });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Hello world!! :D ');
+    });
+
+    it('can change the message', async () => {
+        await inbox.methods.setMessage('Goodbye world!! :(').send({ from: accounts[0]});
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Goodbye world!! :(');
+
+    });
+});
